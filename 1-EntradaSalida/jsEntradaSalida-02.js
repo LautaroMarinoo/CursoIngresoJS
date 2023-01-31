fa072d250fca938c4ad4374c1ab851296b4884a8
LautaroMarinoo <lautaromarinocuentaformal@gmail.com>
2023-01-30 23:24:56 -0300

    segundo cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+//	alert("ok");
 
+//var edad;
+let nombre;
+let nombreCompleto; // notacion lower carnel case
+//nombre = "lautaro"; 
+// asignation de string. 
+nombre =prompt ("ingrese un nombre porfavor");
+//mostrar alert
+alert("nombre")
+}
