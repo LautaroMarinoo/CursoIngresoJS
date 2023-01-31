2b8aa2e2c59cc4f45c64ee43854c5f727132218b
LautaroMarinoo <lautaromarinocuentaformal@gmail.com>
2023-01-30 22:53:17 -0300

    segundo cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
-{ alert("hola soy lautaroo");}
+{ alert("hola soy lautaroooo");}
 
